2a6fb81da7413b6a66c91e5825968a09cec435fa
19suri <suryanshkatoch10@gmail.com>
2024-06-19 14:19:57 +0530

    do itt

--- js/script.js
+++ js/script.js
@@ -1,17 +1,107 @@
-var message = "in global";
-console.log("global: message = " + message);
+/*
+var string = "Hello";
+string += " World";
+string = string + " World";
+console.log(string + "!");
 
-var a = function () {
-  var message = "inside a";
-  console.log("a: message = " + message);
-  function b () {
-    console.log("b: message = " + message); }
-b();
+
+
+
+console.log((5 + 4) / 3);
+console.log(undefined / 5);
+function test1 (a) {
+  console.log( a / 5);
 }
-  
-  
+test1();
+
+
+
 
  
+var x = 4, y = 4;
+if (x == y) {
+  console.log("x=4 is equal to y=4");
+}
+
+x = "4";
+if (x == y) {
+  console.log("x='4' is equal to y=4");
+}
 
 
-a();
+
+
+
+
+if (x === y) {
+  console.log("Strict: x='4' is equal to y=4");
+}
+else {
+  console
+  .log("Strict: x='4' is NOT equal to y=4");
+}
+
+
+
+
+
+
+
+if ( false || null || 
+     undefined || "" || 0 || NaN) {
+  console.log("This line won't ever execute");
+}
+else {
+  console.log ("All false");
+}
+
+
+if (true && "hello" && 1 && -1 && "false") {
+  console.log("All true");
+}
+
+
+
+
+
+
+function a() 
+{
+  return
+  { 
+    name: "Yaakov"
+  };
+}
+
+function b() {
+  return { 
+      name: "Yaakov"
+  };
+}
+
+console.log(a());
+console.log(b());
+
+
+
+var sum = 0;
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+  sum = sum + i;
+}
+console.log("sum of 0 through 9 is: " + sum);
+*/
+
+function orderchickenwith(sidedish){
+  
+  sidedish= sidedish || "whatever";
+  console.log("chicken wiht "+ sidedish);
+}
+
+orderchickenwith('noodles');
+orderchickenwith('');
+
+var x = 10;
+if ( (null) || x > 5 ) {
+  console.log("Hello");
+}
